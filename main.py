874868f79b76832724c9160e2a69e38711eb0c3d
NGine <56292502+mikomac405@users.noreply.github.com>
2021-06-14 19:12:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-class Calculator:
-  
-  @staticmethod
-  def add(a, b):
-    return a+b
-  
-  @staticmethod
-  def sub(a, b):
-    return a-b
-  
-  @staticmethod
-  def mul(a, b):
-    return a*b
-  
-  @staticmethod
-  def div(a, b):
-    return a/b
+def add(a, b):
+  return a+b
+
+
+def sub(a, b):
+  return a-b
+
+
+def mul(a, b):
+  return a*b
+
+
+def div(a, b):
+  return a/b
 
 if __name__ == '__main__':
   print("Build success!")
