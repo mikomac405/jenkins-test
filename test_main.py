4bf4c26764456f809b808866da07d54805c51c2b
NGine <56292502+mikomac405@users.noreply.github.com>
2021-06-14 19:11:38 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,11 +1,11 @@
-from .main import calculator
+import main
 
 class Tests:
   def test_add(self):
-    assert 4 == calculator.add(2, 2)
+    assert 4 == main.add(2, 2)
     
   def test_sub(self):
-    assert 2 == calculator.add(4, 2)
+    assert 2 == main.add(4, 2)
     
   def test_wrong_add(self):
-    assert 10 == calculator.wrong_add(5, 5)
+    assert 10 == main.wrong_add(5, 5)
