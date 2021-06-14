65caffcd31276d43cbb314ee729af47a55ea975c
NGine <56292502+mikomac405@users.noreply.github.com>
2021-06-14 18:43:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Ok")
+class Calculator:
+  
+  @staticmethod
+  def add(a, b):
+    return a+b
+  
+  @staticmethod
+  def sub(a, b):
+    return a-b
+  
+  @staticmethod
+  def mul(a, b):
+    return a*b
+  
+  @staticmethod
+  def div(a, b):
+    return a/b
