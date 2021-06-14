1541133b78d04947a19b42866784be55cb90c361
NGine <56292502+mikomac405@users.noreply.github.com>
2021-06-14 19:15:16 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,7 +5,13 @@ class Tests:
     assert 4 == main.add(2, 2)
     
   def test_sub(self):
-    assert 2 == main.add(4, 2)
+    assert 2 == main.sub(4, 2)
+    
+  def test_mul(self):
+    assert 8 == main.mul(4, 2)
+    
+  def test_div(self):
+    assert 2 == main.div(4, 2)
     
   def test_wrong_add(self):
     assert 10 == main.wrong_add(5, 5)
